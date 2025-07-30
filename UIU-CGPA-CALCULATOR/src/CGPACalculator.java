41b8b305fdb25284eff772bc68b1b3e6eaa7ce7d
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-07-31 02:33:32 +0600

    Update CGPACalculator.java

--- UIU-CGPA-CALCULATOR/src/CGPACalculator.java
+++ UIU-CGPA-CALCULATOR/src/CGPACalculator.java
@@ -1,4 +1,4 @@
-// Import necessary packages..
+// Import necessary packages.
 import javax.swing.*; // For GUI components like JFrame, JButton, etc.
 import java.awt.*; // For layout managers and basic GUI tools.
 import java.awt.event.*; // For handling events like button clicks..
